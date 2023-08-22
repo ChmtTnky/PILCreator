dcb1af8d0d64fa3804a7a30048711af4a857cdf2
ChmtTnky <100511013+ChmtTnky@users.noreply.github.com>
2023-08-22 04:36:19 -0700

    PIM count fixed

--- PILCreator/PILCreator.cs
+++ PILCreator/PILCreator.cs
@@ -19,6 +19,7 @@
 
             // write first 8 bytes of header, then file name with 4 byte alignment, then the rest of the data starting at 0x10 (after the header)
             // repeat for every pim file
+            pil_data[0] = (byte)pim_paths.Length;
             int index = 0x10;
             for (int i = 0; i < pim_paths.Length; i++)
             {
